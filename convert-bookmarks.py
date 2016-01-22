1454b42049e94db896aab99e2dd1b286ca2d04e3
Jeff Hutchison <jeff@jeffhutchison.com>
2016-01-22 06:48:14 -0500

    Add options for add date and tags.

--- convert-bookmarks.py
+++ convert-bookmarks.py
@@ -11,10 +11,11 @@ import json
 parser = ArgumentParser(description='Convert Netscape bookmarks to JSON')
 parser.add_argument(dest='filenames', metavar='filename', nargs='*')
 parser.add_argument('-t', '--tag', metavar='tag', dest='tags',
-                    action='append', help='add tags to bookmarks')
+                    action='append', help='add tag to bookmarks, repeat \
+                                           for multiple tags')
+parser.add_argument('-m', '--mongodb', action='store_true', dest='mongo',
+                    help='output in mongodb import format')
 args = parser.parse_args()
-
-bookmarks = []
 
 for filename in args.filenames:
     soup = BeautifulSoup(open(filename, encoding='utf8'), "html5lib")
@@ -24,8 +25,11 @@ for filename in args.filenames:
         bookmark['title'] = link.string.strip()
         secs = link.get('add_date')
         date = datetime.fromtimestamp(int(secs), tz=timezone.utc)
-        bookmark['add_date'] = { '$date': date.isoformat() }
+        bookmark['add_date'] = {'$date': date.isoformat()} if args.mongo \
+            else date.isoformat()
         bookmark['tags'] = link.get('tags').split(',')
+        if args.tags:
+            bookmark['tags'] += args.tags
         sibling = link.parent.next_sibling
         if sibling and sibling.name == 'dd':
             bookmark['comment'] = sibling.string.strip()
