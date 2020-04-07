345056a7a6a801013cdc340f0f9cd8b4f5d48173
Spenser Truex <web@spensertruex.com>
2020-04-06 20:39:41 -0700

    Remove bson, datetime, and mongo
    Current BSON fails to work
    datetime can't be serialized by json_util
    mongodb is not needed; just use JSON

--- convert-bookmarks.py
+++ convert-bookmarks.py
@@ -4,18 +4,13 @@
 #
 from argparse import ArgumentParser
 from bs4 import BeautifulSoup
-from datetime import datetime, timezone
-from bson import json_util
 import json
-
 
 parser = ArgumentParser(description='Convert Netscape bookmarks to JSON')
 parser.add_argument(dest='filenames', metavar='filename', nargs='+')
 parser.add_argument('-t', '--tag', metavar='tag', dest='tags',
                     action='append', help='add tag to bookmarks, repeat \
                                            for multiple tags')
-parser.add_argument('-m', '--mongodb', action='store_true', dest='mongo',
-                    help='output in mongodb import format')
 args = parser.parse_args()
 
 for filename in args.filenames:
@@ -26,10 +21,6 @@ for filename in args.filenames:
         bookmark['url'] = link.get('href')
         bookmark['title'] = link.string.strip() if link.string\
                                                 else bookmark['url']
-        # add date
-        secs = link.get('add_date')
-        date = datetime.fromtimestamp(int(secs), tz=timezone.utc)
-        bookmark['add_date'] = date
         # tags
         tags = link.get('tags')
         bookmark['tags'] = tags.split(',') if tags else []
@@ -39,9 +30,5 @@ for filename in args.filenames:
         sibling = link.parent.next_sibling
         bookmark['comment'] = \
             sibling.string.strip() if sibling and sibling.name == 'dd' \
-            else ''
-        # make json
-        if args.mongo:
-            print(json_util.dumps(bookmark, sort_keys=False, indent=4))
-        else:
-            print(json.dumps(bookmark, sort_keys=False, indent=4))
+        else ''
+        print(json.dumps(bookmark, sort_keys=False, indent=4))
